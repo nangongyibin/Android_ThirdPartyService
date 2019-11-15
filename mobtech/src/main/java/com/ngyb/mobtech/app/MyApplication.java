5c11d2a59e0262799535b477f34ecfda849e6be9
nangongyibin7219 <nangongyibin@gmail.com>
2019-11-15 15:55:44 +0800

    项目初始化

---
+++ mobtech/src/main/java/com/ngyb/mobtech/app/MyApplication.java
@@ -0,0 +1,16 @@
+package com.ngyb.mobtech.app;
+
+import android.app.Application;
+
+/**
+ * 作者：南宫燚滨
+ * 描述：MobTech平台的第三方服务的简单使用
+ * 邮箱：nangongyibin@gmail.com
+ * 日期：2019/11/15 15:52
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
