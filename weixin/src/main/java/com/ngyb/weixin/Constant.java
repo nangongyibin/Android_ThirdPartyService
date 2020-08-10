2cfbaaaaaefde08312cffababe3e8c09c4d7459d
南宫燚滨 <nangongyibin@gmail.com>
2020-08-11 06:21:37 +0800

    微信支付集成。

---
+++ weixin/src/main/java/com/ngyb/weixin/Constant.java
@@ -0,0 +1,11 @@
+package com.ngyb.weixin;
+
+/**
+ * 作者：南宫燚滨
+ * 描述：
+ * 邮箱：nangongyibin@gmail.com
+ * 日期：2020/8/11 06:09
+ */
+public class Constant {
+    public static final String APP_ID = "wxb4ba3c02aa476ea1";
+}
---
+++ weixin/src/main/java/com/ngyb/weixin/MainActivity.java
@@ -0,0 +1,64 @@
+package com.ngyb.weixin;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.tencent.mm.opensdk.modelpay.PayReq;
+import com.tencent.mm.opensdk.openapi.IWXAPI;
+import com.tencent.mm.opensdk.openapi.WXAPIFactory;
+
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+    private IWXAPI api;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        api = WXAPIFactory.createWXAPI(this, Constant.APP_ID);
+        api.registerApp(Constant.APP_ID);
+    }
+
+    public void pay(View view) {
+        String url = "https://wxpay.wxutil.com/pub_v2/app/app_pay.php";
+        Toast.makeText(MainActivity.this, "获取订单中...", Toast.LENGTH_SHORT).show();
+        try {
+            byte[] buf = Util.httpGet(url);
+            if (buf != null && buf.length > 0) {
+                String content = new String(buf);
+                Log.e("get server pay params:", content);
+                JSONObject json = new JSONObject(content);
+                if (null != json && !json.has("retcode")) {
+                    PayReq req = new PayReq();
+                    //req.appId = "wxf8b4f85f3a794e77";  // 测试用appId
+                    req.appId = json.getString("appid");
+                    req.partnerId = json.getString("partnerid");
+                    req.prepayId = json.getString("prepayid");
+                    req.nonceStr = json.getString("noncestr");
+                    req.timeStamp = json.getString("timestamp");
+                    req.packageValue = json.getString("package");
+                    req.sign = json.getString("sign");
+                    req.extData = "app data"; // optional
+                    Toast.makeText(MainActivity.this, "正常调起支付", Toast.LENGTH_SHORT).show();
+                    // 在支付之前，如果应用没有注册到微信，应该先调用IWXMsg.registerApp将应用注册到微信
+                    api.sendReq(req);
+                } else {
+                    Log.d("PAY_GET", "返回错误" + json.getString("retmsg"));
+                    Toast.makeText(MainActivity.this, "返回错误" + json.getString("retmsg"), Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Log.d("PAY_GET", "服务器请求错误");
+                Toast.makeText(MainActivity.this, "服务器请求错误", Toast.LENGTH_SHORT).show();
+            }
+        } catch (Exception e) {
+            Log.e("PAY_GET", "异常：" + e.getMessage());
+            Toast.makeText(MainActivity.this, "异常：" + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+}
